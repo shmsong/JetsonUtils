470775731ba56a58ccbe7abb8418699f0fff095c
shmsong <shisong@umich.edu>
2021-05-19 10:47:37 -0700

    update torch build

--- jetson-pytorch-docker/scripts/build-pytorch.sh
+++ jetson-pytorch-docker/scripts/build-pytorch.sh
@@ -33,19 +33,7 @@ git checkout v$version -b v$version
 git submodule sync
 git submodule update --init --recursive
 
-if [[ $ONEDNN_BUILD ]]; then
-  # Patch to enable oneDNN (MKL-DNN).
-  patch -p1 < $PACKAGE_DIR/pytorch_onednn.patch
-  export USE_MKLDNN="ON"
-
-  case $ONEDNN_BUILD in
-    reference )
-    ;;
-    acl )
-    export USE_MKLDNN_ACL="ON"
-    ;;
-  esac
-fi
+export USE_MKLDNN="ON"
 
 # Update the oneDNN tag in third_party/ideep
 cd third_party/ideep/mkl-dnn
@@ -53,18 +41,4 @@ git checkout $ONEDNN_VERSION
 
 cd $PACKAGE_DIR/$src_repo
 
-MAX_JOBS=${NP_MAKE:-$((num_cpus / 2))} OpenBLAS_HOME=$OPENBLAS_DIR/lib CXX_FLAGS="$BASE_CFLAGS -O3" LDFLAGS=$BASE_LDFLAGS USE_OPENMP=1 USE_LAPACK=1 USE_CUDA=0 USE_FBGEMM=0 USE_DISTRIBUTED=0 python setup.py install
-
-# Check the installation was sucessful
-cd $HOME
-
-# Check the wheel has installed correctly.
-# Note: only checks the major.minor version numbers, and not the point release
-check_version=$(python -c 'import torch; print(torch.__version__)' | cut -f1,2 -d'.')
-required_version=$(echo $version | cut -f1,2 -d'.')
-if [[ "$check_version" == "$required_version" ]]; then
-  echo "PyTorch $required_version package installed."
-else
-  echo "PyTorch package installation failed."
-  exit 1
-fi
+MAX_JOBS=${NP_MAKE:-$((num_cpus / 2))} OpenBLAS_HOME=$OPENBLAS_DIR/lib CXX_FLAGS="$BASE_CFLAGS -O3" LDFLAGS=$BASE_LDFLAGS USE_OPENMP=1 USE_LAPACK=1 USE_CUDA=1 USE_FBGEMM=0 USE_DISTRIBUTED=0 python setup.py install
